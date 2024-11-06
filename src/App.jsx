60546888ce4c67a50c65cb0cc93998595fd03a7d
Sukhveer <techforindia03@gmail.com>
2024-11-06 10:16:19 +0530

    Resolves issue where #workout anchor did nott work on first click

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Hero from "./components/Hero";
 import Workout from "./components/Workout";
 import Generator from "./components/Generator";
-import { useState } from 'react';
+import { useState } from "react";
 import { generateWorkout } from "./utils/functions";
-
+import { useEffect } from "react";
 
 const App = () => {
   const [workout, setWorkout] = useState(null);
@@ -12,15 +12,18 @@ const App = () => {
   const [goal, setGoal] = useState("strength_power");
 
   function updateWorkout() {
-    if(muscles.length < 1){return}
-    let newWorkout = generateWorkout({poison, goal, muscles})
-   console.log(newWorkout);
-   
+    if (muscles.length < 1) {
+      return;
+    }
+    let newWorkout = generateWorkout({ poison, goal, muscles });
+    console.log(newWorkout);
+
     setWorkout(newWorkout);
-    
-        window.location.href = "#workout";
-    
   }
+  useEffect(() => {
+    window.location.href = "#workout";
+  }, [workout]);
+
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
@@ -39,8 +42,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
